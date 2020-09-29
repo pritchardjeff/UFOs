7f6a5344d02192ce78e053eaa17c7feff9ce26cd
pritchardjeff <68468033+pritchardjeff@users.noreply.github.com>
2020-09-29 05:59:31 -0400

    Button Click Function

--- static/JavaScript/app.js
+++ static/JavaScript/app.js
@@ -22,3 +22,25 @@ function buildTable(data) {
         );       
     }); 
 }
+
+// Button Function
+function handleClick() {
+    // finds the #datetime id in HTML
+    // grabs the nested value & stores as date variable
+    let date = d3.select("#datetime").property("value");
+    // sets the filtered data =  to our raw data 
+    // start with a blank slate
+    let filteredData = tableData;
+    // Show only rows where the date is equal to the date filter we created above
+    // ==== strict equality (pefect match) == loose equality (loosly matched/fuzzylookup?) 
+    if (date)  {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    //Rebuild table using filtered data (if no filter, you get original data)
+    buildTable(filteredData);
+}
+// Attach an event to listen for the form button (in this case a click on the filter)
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build the table when the table loads
+buildTable(tableData);
